c3accf7c2c41f59d92697a154d169101440caa70
JonathanGrant <jonathanallengrant@gmail.com>
2016-09-17 08:18:10 -0700

    Ability to follow ig users

--- apithree/index.js
+++ apithree/index.js
@@ -3,6 +3,12 @@ var app = express();
 var https = require('https');
 var FormData = require('form-data');
 var util = require('util');
+var bodyParser = require('body-parser')
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get('/oauth/ig', function (req, res) {
   var form = new FormData();
@@ -12,11 +18,11 @@ app.get('/oauth/ig', function (req, res) {
   form.append('redirect_uri','http://localhost:1992/oauth/ig')
   form.append('code',req.query.code)
   form.submit({hostname: "api.instagram.com", path: "/oauth/access_token", protocol: 'https:'}, (error, response) => {
-    console.log(error)
+    console.log(error);
     console.log(`STATUS: ${response.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-    console.log(util.inspect(response))
-    console.log(JSON.stringify(response.body))
+    console.log(util.inspect(response));
+    console.log(JSON.stringify(response.body));
     var body = "";
     response.on('readable', function() {
         body += response.read();
@@ -27,6 +33,24 @@ app.get('/oauth/ig', function (req, res) {
     });
   });
 })
+
+app.post('/ig/follow', function(req, res) {
+    console.log(req.query.friend_id)
+    theAccess = req.query.access_token
+    var form = new FormData();
+    form.append('access_token', theAccess);
+    form.append('action', 'follow');
+    form.submit({hostname: "api.instagram.com", path: `/v1/users/${req.query.friend_id}/relationship?access_token=${theAccess}`, protocol: 'https:'}, (error, response) => {
+      var body = "";
+      response.on('readable', function() {
+          body += response.read();
+      });
+      response.on('end', function() {
+          console.log(body);
+          res.send(body);
+      });
+    });
+});
 
 var server = app.listen(1992, function () {
 
