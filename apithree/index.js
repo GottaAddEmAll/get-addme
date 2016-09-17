d89750eb74c73c49f642d087342010c8f82210ab
estermer <eric.stermer@gmail.com>
2016-09-17 15:33:35 -0400

    Added a get request for GitHub oauth
    *needs an accept header as per docs to change results into JSON

--- apithree/index.js
+++ apithree/index.js
@@ -73,6 +73,25 @@ app.get('/oauth/fb', function (req, res) {
   	});
 })
 
+app.get('/oauth/gh', function (req, res) {
+
+    https.get('https://github.com/login/oauth/access_token?client_id=89221658f77bf282f490&client_secret=11f6d9c96c884834e4d3c4cfc0b8cd5c32231c52&code=' + req.query.code + '&redirect_uri=http://localhost:1992/oauth/gh', (response) => {
+
+	    console.log(req.query.code);
+	   	console.log(response);
+
+	    var body = "";
+	    response.on('readable', function() {
+	        body += response.read();
+	    });
+	    response.on('end', function() {
+	        console.log(body);
+	        res.send(body);
+	    });
+
+  	});
+})
+
 app.post('/snapchat/save/:snapchat_id/', function(req, res) {
   console.log("Save Snapchat Id");
   // Get user calling function
