4bcbf56f179ee7266d8e0459e17cc040f6b75bb3
JonathanGrant <jonathanallengrant@gmail.com>
2016-09-16 22:32:14 -0700

    Started serevr

--- addme/index.ios.js
+++ addme/index.ios.js
@@ -17,10 +17,10 @@ class addme extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          AddMe
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Press here to generate code
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
