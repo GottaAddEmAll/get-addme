000af98c5567b800037d990ea04a693586eef6d4
estermer <eric.stermer@gmail.com>
2016-09-17 14:24:12 -0400

    re commited uncommmited files

---
+++ apithree/app-codes.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'instagramAuth' : {
+        'clientID'      : 'fc50049ba7df49b7b96535f892642366', // your App ID
+        'clientSecret'  : '6658e18d25e740e691c0cdcdd9adeabf', // your App Secret
+        'callbackURL'   : 'http://localhost:1992/oauth/ig'
+    },
+
+    'facebookAuth' : {
+        'clientID'          : '1779030152315570',
+        'clientSecret'      : 'e76f45468d975b274e3d3548066e88db',
+        'callbackURL'       : 'http://localhost:1992/oauth/fb'
+    },
+
+    'twitterAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:1992/oauth/tw'
+    }
+
+};
